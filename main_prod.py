d6c8710dd77e5c08d862b66cceb58551ba9a97c1
Mariano <mariano7717@gmail.com>
2024-06-03 10:59:55 +0100

    commit

---
+++ main_prod.py
@@ -0,0 +1,11 @@
+# SLU-tty Python version of the remote terminal session manager
+
+# 2024june6
+# initial draft of the application
+
+def menu():
+    print("-------------------<  SLU-tty  >-------------------"
+          " SSH hosts Listing and Unified credential manager  "
+          "---------------------------------------------------"
+          ""
+          "")
