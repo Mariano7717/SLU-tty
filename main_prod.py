a3d71c248e9dd878215f7814b9fdcad06f651a3d
Mariano Martínez Martínez <42985199+Mariano7717@users.noreply.github.com>
2024-06-03 20:02:38 +0100

    Update main_prod.py

--- main_prod.py
+++ main_prod.py
@@ -3,9 +3,51 @@
 # 2024june6
 # initial draft of the application
 
-def menu():
-    print("-------------------<  SLU-tty  >-------------------"
-          " SSH hosts Listing and Unified credential manager  "
-          "---------------------------------------------------"
-          ""
-          "")
+# switch control for the desired action to perform
+# connect to a ssh host
+# edit add remove ssh host
+# edit add remove credentials
+
+def startmenu():
+    banner = ("|||||||||||||||||||||||||||||||||||||||||||||||||||\n"
+              "|----------------<   SLU-tty    >-----------------|\n"
+              "|----------------<  START MENU  >-----------------|\n"
+              "|SSH hosts Listing and Unified credential manager |\n"
+              "|-------------------------------------------------|\n"
+              "|                                                 |\n"
+              "| Select the option that applies and hit ENTER    |\n"
+              "| 1. Connect to a host                            |\n"
+              "| 2. Edit hosts                                   |\n"
+              "| 3. Edit credentials                             |\n"
+              "|                                                 |\n"
+              "|||||||||||||||||||||||||||||||||||||||||||||||||||\n")
+
+    print(banner)
+
+    # switch case and call functions
+
+    switch_startmenu = input()
+    # wait on input
+
+    if switch_startmenu == "1":
+        (hostconnect())
+    elif switch_startmenu == "2":
+        (edithost())
+    elif switch_startmenu == "3":
+        (editcreds())
+
+
+def hostconnect():
+    import os
+    os.system("start cmd.exe")
+
+def edithost():
+    print("-----------------<  EDIT HOSTS  >------------------\n"
+          " Edit your host information here \n"
+          "---------------------------------------------------\n")
+def editcreds():
+    print("-----------------<  EDIT CREDS  >------------------\n"
+          " Edit your Credentials information here \n"
+          "---------------------------------------------------\n")
+
+startmenu()
